f986661c51100b1a33a2336af9e944f807fbee68
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2026-03-15 19:20:02 +0530

    Add personal and API tokens to demo.sh
    Added various personal and API tokens to demo.sh

--- demo.sh
+++ demo.sh
@@ -1,2 +1,8 @@
 Hemadrio : ghp_WqsT4BBzj9vJp1AoS9yOSiHQAahboQ2cliLM
 ALL ACCESS token - ghp_y7nXx6ghNTwzXInKkwcnWhQ4uNeS8f4AQvbO
+Doopler Personal Token : dp.pt.ez6LmLY0iXIPrSQKwo4r6El7Wt9SrVqVg9uyeEwbIMM
+Fastly Personal Token : _xxcdWcRJjRkvgarRI7NzDZgDu1e0jwM
+Fastly Automation Token : 0e7iGN9OT--qze34SlKobz7tKf5SK5rM
+FinHub API Key : cn04751r01qkcvkfndb0cn04751r01qkcvkfndbM
+Flickr APP Key : c0f16e29e336b104020d673e49c276b9
+Flickr Secret : 7aa365dd97921da3
