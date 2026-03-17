366b02ffaafb5078e8309b82749f4a75e6f48c83
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2026-03-17 18:59:09 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -6,3 +6,10 @@ Fastly Automation Token : 0e7iGN9OT--qze34SlKobz7tKf5SK5rM
 FinHub API Key : cn04751r01qkcvkfndb0cn04751r01qkcvkfndbM
 Flickr APP Key : c0f16e29e336b104020d673e49c276b9
 Flickr Secret : 7aa365dd97921da3
+hello = "JDF@12`~F@7!qZ#2"
+password = P$5aRt#2oN!9lM@8
+password = "P$5aRt#2oN!9lM@8"
+token= JP$5aGt#4oN!9lM@8AS
+token= "JP$5aGt#4oN!9lM@8AS"
+token = "hem1234$%$**AZkn@145789}|~"
+token= "3DS$5aGt#4oN!9lM@8AS"
